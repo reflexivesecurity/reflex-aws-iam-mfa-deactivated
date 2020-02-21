dee555978641b452449fe1a89a54189f6875ae38
rjulian <richard@rjulian.net>
2020-02-21 13:54:34 -0800

    Initial commit

---
+++ detect_deactivate_mfa.tf
@@ -0,0 +1,28 @@
+module "detect_deactivate_mfa" {
+  source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_sns_email?ref=v0.0.1"
+  rule_name        = "DetectMFADeactivate"
+  rule_description = "Rule to check when MFA Devices are Deactivated"
+
+  event_pattern = <<PATTERN
+{
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "source": [
+    "aws.iam"
+  ],
+  "detail": {
+    "eventSource": [
+      "iam.amazonaws.com"
+    ],
+    "eventName": [
+      "DeactivateMFADevice"
+    ]
+  }
+}
+PATTERN
+
+  topic_name = "DetectDeactivateMFA"
+  target_id  = "DetectDeactivateMFA"
+  email      = var.email
+}
