027e7f8b1f5466bb3dfb13adfe6163515ce3332d
Max Anderson <maxwell.c.anderson@gmail.com>
2020-03-11 09:14:39 -0700

    Point to dev branch

--- detect_deactivate_mfa.tf
+++ detect_deactivate_mfa.tf
@@ -1,5 +1,5 @@
 module "detect_deactivate_mfa" {
-  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.1"
+  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=dev-optional-lambda-policy"
   rule_name        = "DetectDeactivateMFA"
   rule_description = "Rule to check when MFA Devices are Deactivated"
 
