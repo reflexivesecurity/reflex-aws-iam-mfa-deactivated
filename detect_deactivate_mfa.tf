5763100bb037312f325105fe881c48a0b159071d
Max Anderson <maxwell.c.anderson@gmail.com>
2020-03-11 09:54:06 -0700

    Remove lambda policy (#5)
    Patch: Remove custom_lambda_policy

--- detect_deactivate_mfa.tf
+++ detect_deactivate_mfa.tf
@@ -1,5 +1,5 @@
 module "detect_deactivate_mfa" {
-  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.1"
+  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.2"
   rule_name        = "DetectDeactivateMFA"
   rule_description = "Rule to check when MFA Devices are Deactivated"
 
@@ -29,22 +29,6 @@ PATTERN
   environment_variable_map = {
     SNS_TOPIC = var.sns_topic_arn
   }
-  custom_lambda_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:GetEncryptionConfiguration"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-
-
 
   queue_name    = "DetectDeactivateMFA"
   delay_seconds = 60
