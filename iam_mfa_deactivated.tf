f1f6d2328068abeaddf3e2239483e533108ff423
Chris Coleman <chris@rot-26.com>
2020-04-12 13:13:49 -0600

    Renamed rule name: DetectDeactivateMfa IamMfaDeactivated

--- iam_mfa_deactivated.tf
+++ iam_mfa_deactivated.tf
@@ -1,6 +1,6 @@
 module "iam_mfa_deactivated" {
   source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.7"
-  rule_name        = "DetectDeactivateMFA"
+  rule_name        = "IamMfaDeactivated"
   rule_description = "Rule to check when MFA Devices are Deactivated"
 
   event_pattern = <<PATTERN
@@ -22,7 +22,7 @@ module "iam_mfa_deactivated" {
 }
 PATTERN
 
-  function_name   = "DetectDeactivateMFA"
+  function_name   = "IamMfaDeactivated"
   source_code_dir = "${path.module}/source"
   handler         = "iam_mfa_deactivated.lambda_handler"
   lambda_runtime  = "python3.7"
@@ -30,10 +30,10 @@ PATTERN
     SNS_TOPIC = var.sns_topic_arn
   }
 
-  queue_name    = "DetectDeactivateMFA"
+  queue_name    = "IamMfaDeactivated"
   delay_seconds = 0
 
-  target_id = "DetectDeactivateMFA"
+  target_id = "IamMfaDeactivated"
 
   sns_topic_arn  = var.sns_topic_arn
   sqs_kms_key_id = var.reflex_kms_key_id
